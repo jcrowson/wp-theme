c61b67ce76d4ab9d67768c6e8eb83b47cd74721e
James <crowson.j@gmail.com>
2019-02-22 20:17:09 -0500

    Fix newsletter tutorials.

--- newsletter.php
+++ newsletter.php
@@ -9,12 +9,11 @@
         <div class="col-md-12 text-center">
           <h1>The Best React Tutorials, Packed into One Monthly Newsletter</h1>
           <h2 class="pt-0 pb-0 mb-0 d-none d-md-block">
-            Get our latest tutorials sent straight to your inbox each month. Never miss the chance to up your React and JavaScript game!
+            Get our latest tutorials sent straight to your inbox each month.
           </h2>
         </div>
         <div class="col-12 text-center mt-0 mt-md-5">
-          <p class="d-block d-md-none">Get our monthly newsletter, packed full of the latest React tutorials, delivered straight to your inbox.</p>
-          <p class="d-none d-md-block text-muted">We'll never send you spam, just great tutorials. Pop your email in the box below to sign up.</p>
+          <p class="d-block d-md-none">Get our latest tutorials sent straight to your inbox each month.</p>
           <form action="https://wplogic.us18.list-manage.com/subscribe/post?u=7806e537fd7c0d111ed32b4a6&amp;id=c4ae659733" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" validate>
             <div class="input-group mb-3">
               <input type="email" class="form-control" name="EMAIL" id="mce-EMAIL" placeholder="Your Email Address" required>
@@ -23,6 +22,7 @@
               </div>
             </div>
           </form>
+          <p class="d-none d-md-block text-muted">Never spam, just great tutorials. Unsubscribe at any time.</p>
         </div>
       </div>
     </div>
@@ -31,11 +31,11 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <h3 class="pb-0 text-center text-md-left text-muted">Get Great Tutorials Like These, Each Month...</h3>
+          <h3 class="pb-0 text-center text-md-left text-muted">Get great tutorials like these...</h3>
         </div>
       </div>
       <div class="row mt-4">
-        <?php include('blog-list.php') ?>
+        <?php include('all-react-tutorials.php') ?>
       </div>
     </div>
   </div>
