d4c41106a67b6a75847453caacc313bc304ec1d1
James <crowson.j@gmail.com>
2019-04-02 19:09:16 -0400

    Fix WP_Query to return all tutorials, and not just 10.

--- all-react-tutorials.php
+++ all-react-tutorials.php
@@ -2,6 +2,7 @@
   $args = array(
     'category_name' => 'tutorials',
     'tag' => 'react',
+    'posts_per_page' => -1,
   );
   $post_query = new WP_Query($args);
   if($post_query->have_posts() ) {
--- react-tutorials.php
+++ react-tutorials.php
@@ -4,6 +4,7 @@
     'tag' => 'react',
     'meta_key' => 'featured_on_homepage',
     'meta_value' => 0,
+    'posts_per_page' => -1,
   );
   $post_query = new WP_Query($args);
   if($post_query->have_posts() ) {
