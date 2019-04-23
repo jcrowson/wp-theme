b32471bdebedf9f23fa837fdfe906454cfa6d6dd
James <crowson.j@gmail.com>
2019-04-23 14:46:17 -0400

    Add related tutorials to sidebar.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="container">
           <div class="row">
             <div class="col-12 col-md-6 text-muted">
-              <p>Upmostly brings you original React and JavaScript tutorials each week. Learn React and JavaScript the right way.</p>
+              <p>Upmostly brings you original React and JavaScript tutorials each week. Level up your React skills.</p>
             </div>
             <div class="col-12 col-md-6">
               <p>📮 Get the monthly newsletter, packed full of our latest React tutorials, delivered straight to your inbox.</p>
@@ -10,13 +10,13 @@
                 <div class="input-group mb-3">
                   <input type="email" class="form-control" name="EMAIL" id="mce-EMAIL" placeholder="Your Email Address" required>
                   <div class="input-group-append">
-                    <button class="btn btn-primary" type="submit">Join Newsletter</button>
+                    <button class="btn btn-primary" type="submit" onClick="ga('send', 'event', 'cta', 'Click', 'Footer Newsletter', '0');">Join Newsletter</button>
                   </div>
                 </div>
               </form>
             </div>
             <div class="col-12 text-muted">
-              <p>© Upmostly 2019</p>
+              <small>Copyright &copy; 2018 – <?php echo date('Y'); ?> Upmostly</small>
             </div>
           </div>
         </div>
--- single.php
+++ single.php
@@ -15,23 +15,29 @@
 
   <div class="page-content">
     <div class="content">
-
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-8 offset-lg-2 col-xl-4 offset-xl-4">
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <div class="col-sm-12 col-md-12 col-xl-6">
             <?php the_content(); ?>
             <?php endwhile; else : ?>
             <?php endif; ?>
           </div>
-          <div class="col-xl-4">
+          <div class="d-none d-xl-block col-xl-4">
             <div class="floating-wrapper">
               <div class="floating-related-post">
-                You might also like...
-                <div class="featured-card card mt-2 mb-4">
-                  <a href="<?= the_permalink(); ?>">
-                    <img class="card-img-top featured-card-img" src="<?= get_bloginfo('wpurl') . '/wp-content/uploads/' .get_post_meta($post->ID, 'thumbnail', true); ?>" alt="Card Image">
-                  </a>
-                </div>
+                <span class="text-muted">
+                  Related Tutorials 👇
+                </span>
+                  <?php
+                    $post_objects = get_field('related_tutorials');
+                    if( $post_objects ): ?>
+                        <?php foreach( $post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+                            <?php setup_postdata($post); ?>
+                            <?php include('Components/related-tutorial-card.php'); ?>
+                        <?php endforeach; ?>
+                        <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+                    <?php endif;
+                  ?>
               </div>
             </div>
           </div>
@@ -51,7 +57,7 @@
             <div class="input-group my-3">
               <input type="email" class="form-control" name="EMAIL" id="mce-EMAIL" placeholder="Your Email Address" required>
               <div class="input-group-append">
-                <button class="btn btn-primary" type="submit">Subscribe</button>
+                <button class="btn btn-primary" type="submit" onClick="ga('send', 'event', 'cta', 'Click', 'In Page Newsletter', '0');">Subscribe</button>
               </div>
             </div>
             <small>Zero spam, just great content. Unsubscribe at <i>any</i> time.</small>
@@ -60,7 +66,7 @@
       </div>
     </div>
 
-    <div class="container">
+    <div class="container d-xl-none">
       <div class="row">
         <div class="col text-muted">
           <h3>👉 Related Tutorials</h3>
@@ -71,14 +77,14 @@
           $currentPostId = get_the_ID();
           $args = array(
             'posts_per_page' => 3,
-        'post_type' => 'post',
-        'post__not_in' => array($currentPostId)
-        );
-        $post_query = new WP_Query($args);
-        if($post_query->have_posts() ) {
-        while($post_query->have_posts() ) {
-        $post_query->the_post();
-        ?>
+            'post_type' => 'post',
+            'post__not_in' => array($currentPostId)
+          );
+          $post_query = new WP_Query($args);
+          if($post_query->have_posts() ) {
+          while($post_query->have_posts() ) {
+          $post_query->the_post();
+          ?>
         <?php include('Components/tutorial-card.php'); ?>
         <?php
             }
