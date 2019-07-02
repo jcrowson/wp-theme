c7c77438ec3dbe6411cda51a2da28d8a088b5ae4
James <crowson.j@gmail.com>
2019-07-02 08:01:25 -0400

    Up jquery version

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
   function main_script_create() {
     wp_enqueue_style('bootstrap4', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '1.0.0', 'all');
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/assets/code.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/assets/code.css', array(), '1.0.1', 'all');
     wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css', array(), '1.0.0', 'all');
-    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', array(), null, true);
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', array(), null, true);
     wp_enqueue_script('bootstrapjs', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array(), null, true);
   }
   add_action('wp_enqueue_scripts', 'main_script_create');
