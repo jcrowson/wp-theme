df2bbfc3365ced35f97f20d7dbec55b4016dee20
James <TJVvmnsNzH6{Ptbin6PDdDGi>
2019-10-01 17:44:25 -0400

    Add most read block

---
+++ Components/author-section.php
@@ -0,0 +1,11 @@
+<div class="row">
+  <div class="col-12 p-0">
+    <div class="media author-block">
+      <img class="rounded-circle mr-3" src="https://upmostly.com/wp-content/uploads/james-headshot.jpg" alt="James King headshot">
+      <div class="media-body">
+        <h5 class="author mt-0">👋 Hey, I'm James King</h5>
+        My tutorials help 100,000+ developers learn React and JavaScript every month. If you'd like to receive a friendly email once in a while containing new tutorials and insights, you can sign up above. I'd appreciate the support!
+      </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
       <div class="row">
         <div class="col-md-12 text-center">
           <h1 class="hero-text">Level Up Your React Skills</h1>
-          <h2 class="pt-0 pb-0 mb-0 hero-subtitle-text">
+          <h2 class="pt-0 pb-0 mb-0 no-underline">
             Upmostly brings you original React and JavaScript tutorials each week.
           </h2>
         </div>
